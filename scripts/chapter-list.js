059884e86a9eb5594259781760061e373a8ae8dc
Petr Novák <novakpetr531@gmail.com>
2025-04-17 17:13:55 +0200

    Dynamická úprava názvu kapitol podle výběru ze seznamu

--- scripts/chapter-list.js
+++ scripts/chapter-list.js
@@ -1,5 +1,6 @@
 var chapters = [
     [1, "Úvod"],
+    [1.1, "Úvod do programování"],
     [2, "Základy"],
     [3, "Pokročilé techniky"],
     [4, "Závěr"]
@@ -7,9 +8,21 @@ var chapters = [
 
 function insertChapters(){
     var chaptersList = document.getElementById('chapter-list');
+
     let chaptersData = chapters.map(item =>
-        `<li><a href='chapter${item[0]}.html'>${item[0]} - ${item[1]}</a></li>`).join('');
+        `<li onclick="loadChapter(${item[0]})"><a > ${item[0]} - ${item[1]}</a></li>`).join('');
     
     console.log(chaptersData);
     chaptersList.innerHTML = chaptersData;
+}
+
+
+function loadChapter(chapter){
+    console.log(`Loading chapter ${chapter} ...`);
+
+    let title = `${chapter} - ${chapters.find(item => item[0] === chapter)[1]}`;
+    let chapterTitle = document.getElementById('chapter-title');
+    chapterTitle.innerHTML = title;
+
+    let videoElement = document.querySelector('video');
 }
--- scripts/video.js
+++ scripts/video.js
@@ -16,4 +16,3 @@ function insertVideo() {
     videoElement.appendChild(source);
     videoElement.load();
 }
-
